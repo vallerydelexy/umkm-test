7d6d57a04e6cf8a384738a01ffe865e25de87528
vallerydelexy <vallerydelexy@gmail.com>
2024-01-20 23:14:37 +0700

    vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-import './bootstrap';
+import "./bootstrap";
+import { createApp } from "vue";
+import Home from "./components/Home.vue";
+const app = createApp({
+    components: {
+        Home,
+    },
+});
+app.mount("#app");
---
+++ resources/js/components/Home.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1>netwerk umkm test - Rizki Aprita</h1>
+</template>
